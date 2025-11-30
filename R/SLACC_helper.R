40bd4079f384e764fe96163eeddd81513b433e07
junjypark <junjy.park@utoronto.ca>
2025-11-30 00:44:32 -0500

    update namespace

--- R/SLACC_helper.R
+++ R/SLACC_helper.R
@@ -2,18 +2,18 @@ Ltrans = function(X,d = TRUE){ X[upper.tri(X,d)]  }
 
 Ltrinv = function(x,V,d = TRUE){ Y = matrix(0,ncol = V,nrow = V);
 Y[upper.tri(Y,d)]=x;return(Y + t(Y) - d*diag(diag(Y)))  }
-
-estim_phi2 = function(input, batch, nonzero){
-  phi2 = vector()
-  ni = table(batch)
-  names = unique(batch)
-  n.batch = length(ni)
-  for (i in 1:n.batch){
-    index = which(batch==names[i])
-    phi2[i] = estim_sigma(input[index,nonzero,drop=FALSE],method="MAD")^2
-  }
-  return(phi2)
-}
+# 
+# estim_phi2 = function(input, batch, nonzero){
+#   phi2 = vector()
+#   ni = table(batch)
+#   names = unique(batch)
+#   n.batch = length(ni)
+#   for (i in 1:n.batch){
+#     index = which(batch==names[i])
+#     phi2[i] = estim_sigma(input[index,nonzero,drop=FALSE],method="MAD")^2
+#   }
+#   return(phi2)
+# }
 
 HOSVD_initial=function(Y, L, X, batch, nonzero){
   n = nrow(Y)
@@ -39,9 +39,8 @@ HOSVD_initial=function(Y, L, X, batch, nonzero){
   sigma2_ini=foreach(g=1:length(unique(batch)),.combine="rbind")%do%{
     apply(resid[groups[[g]],], 2,var)
   }
-  # R_ini = cor(resid)
-  phi2_ini = estim_phi2(Y - A_ini %*% t(S_ini), batch, nonzero)
-  # return(list(U = U_ini, S = S_ini, A = A_ini, phi2 = phi2_ini, B = B, sigma2=sigma2_ini, R=R_ini))
+  E=Y - A_ini %*% t(S_ini)
+  phi2_ini = rep(var(c(E[,nonzero])),length(unique(batch)))
   return(list(U = U_ini, S = S_ini, A = A_ini, phi2 = phi2_ini, B = B, sigma2=sigma2_ini))
 }
 
