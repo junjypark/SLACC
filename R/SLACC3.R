791de78917429bec2aaa1de4f740df61b36026ca
junjypark <junjy.park@utoronto.ca>
2025-12-04 00:45:22 -0500

    Update SLACC3.R

--- R/SLACC3.R
+++ R/SLACC3.R
@@ -1,4 +1,4 @@
-SLACC3 = function(dat, mod = NULL, L = 5, batch = NULL, maxIter = 20, eps = 1e-4, ADMM_maxIter = 100, ADMM_eps = 1e-3, include_diag=T, init = NULL,
+SLACC3 = function(dat, mod = NULL, L = 5, batch = NULL, maxIter = 20, eps = 1e-4, U_maxIter = 100, U_eps = 1e-3, include_diag=T, init = NULL,
                  lambda_U = NULL, lambda_BIC=NULL, tau = NULL, harmonize = TRUE){
   n = nrow(dat); p = ncol(dat); V = (sqrt(1+8*p)-1)/2;
   
@@ -58,7 +58,11 @@ SLACC3 = function(dat, mod = NULL, L = 5, batch = NULL, maxIter = 20, eps = 1e-4
     #M step - update U
     prep = prepare_elements(dat, A=A, U=U, L=L, phi2=phi2_g, tau=tau, ni=ni)
     w = prep$subj_wts
-    U = bilinear_admm3(Y = prep$Y, A = prep$X, w = prep$subj_wts, Q = Q_list, groups=groups, C = prep$B_wts, U0=U, V0=U, lambda = lambda_U/2, maxit = ADMM_maxIter, tol = ADMM_eps, include_diag=include_diag)$U
+    if (lambda>0){
+      U = bilinear_admm3(Y = prep$Y, A = prep$X, w = prep$subj_wts, Q = Q_list, groups=groups, C = prep$B_wts, U0=U, V0=U, lambda = lambda_U/2, maxit = U_maxIter, tol = U_eps, include_diag=include_diag)$U
+    } else if (lambda==0){
+      U = bilinear_als3(Y = prep$Y, A = prep$X, w = prep$subj_wts, Q = Q_list, groups = groups, U0 = U, V0 = U, include_diag = include_diag, maxit = U_maxIter, tol = U_eps )$U
+    }
     
     for (l in 1:L){
       sc = sqrt(sum(U[,l]^2))
